0912efd45d116d2589dd2ddf27bb89289bac1726
Sanpi <sanpi@homecomputing.fr>
2013-02-14 08:08:07 +0100

    Delete useless scripts

--- pelicanconf.py
+++ pelicanconf.py
@@ -6,20 +6,14 @@ SITEURL = 'http://sanpi.homecomputing.fr'
 
 TIMEZONE = 'Europe/Paris'
 
-DEFAULT_LANG = u'fr'
-
 STATIC_PATHS = (['images', 'documents'])
-
-THEME = 'iris'
 
 DEFAULT_PAGINATION = 10
 
 DISPLAY_PAGES_ON_MENU = False
 
-ARTICLE_URL = "content/{slug}"
-ARTICLE_SAVE_AS = "content/{slug}/index.html"
-
-PATH = 'src'
+ARTICLE_URL = "{category.slug}/{slug}"
+ARTICLE_SAVE_AS = "{category.slug}/{slug}/index.html"
 
 # Social widget
 LINKS =  ()
