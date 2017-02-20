445c37ec170f5f8008ce28e924e4c0874cf18072
Sanpi <sanpi@homecomputing.fr>
2017-02-20 11:17:07 +0100

    Remove piwik url

--- settings.py
+++ settings.py
@@ -39,7 +39,3 @@ FEED_DOMAIN = 'https://sanpi.homecomputing.fr'
 FEED_RSS = 'all.rss.xml'
 TAG_FEED_RSS = 'feeds/tags/%s.rss.xml'
 CATEGORY_FEED_RSS = 'feeds/categories/%s.rss.xml'
-
-PIWIK_URL = 'https://s.homecomputing.fr'
-PIWIK_ONION = 'http://s.sanpii6qqjdzws77.onion'
-PIWIK_SITE_ID = 3
